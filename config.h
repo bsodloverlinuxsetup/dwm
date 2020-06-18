a4b0682318e86b19b32abf5511bc0f21edd1d989
Your Name <you@example.com>
2020-06-18 22:32:31 +0200

    added Pango

--- config.h
+++ config.h
@@ -11,6 +11,7 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int focusonwheel       = 0; 			/*focus on wheel*/
 static const char *fonts[]          = { "Roboto:size=13", "FontAwesome:pixelsize=16:antialias=true:autohint=true"  };
 static char dmenufont[]             = "Roboto:size=13";
 static char normbgcolor[]           = "#222222";
